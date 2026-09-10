550c6705f6de88e7d6cfe1fe9239288412fcf2d9
nandanaj2006 <nandanaj2103@gmail.com>
2026-09-10 14:37:13 +0530

    Add ResourceUnavailableException class

---
+++ src/main/java/com/emergency/exception/ResourceUnavailableException.java
@@ -0,0 +1,5 @@
+package com.emergency.exception;
+
+public class ResourceUnavailableException extends RuntimeException {
+    public ResourceUnavailableException(String message) { super(message); }
+}
